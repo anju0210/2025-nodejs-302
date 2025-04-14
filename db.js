576085a39d99f7721a63dbc4fe47ec4436edb049
anju0210 <s2309@e-mirim.hs.kr>
2025-04-14 11:09:45 +0900

    createPool로 대체

--- db.js
+++ db.js
@@ -1,18 +1,10 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({   // 별도로 connect할 필요 X
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME
-});
-
-db.connect(err=>{
-    if(err){
-        console.error('MySQL 연결 실패 : ', err);
-        return;
-    }
-    console.log('MySQL 연결 성공');
 });
 
 module.exports = db;
